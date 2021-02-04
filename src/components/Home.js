56eb987b9f2386787f927d98598e8cad6e57d405
PhoenixCreation <phs28052001@gmail.com>
2021-02-04 20:40:29 +0530

    some twicks

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,29 +14,30 @@ export default function Home() {
     <div className="home">
       <h1 className="home__header">Pinger. . . .</h1>
       <div className="home__greet">{user?.name}</div>
-      {user?.name && (
+      {user?.name ? (
         <button className="home__auth__loginBtn" onClick={() => logout()}>
           LogOut
         </button>
+      ) : (
+        <div className="home__auth">
+          <div className="home__auth__login">
+            <button
+              className="home__auth__loginBtn"
+              onClick={() => goto("/login")}
+            >
+              Login
+            </button>
+          </div>
+          <div className="home__auth__signup">
+            <button
+              className="home__auth__signupBtn"
+              onClick={() => goto("/signup")}
+            >
+              Signup
+            </button>
+          </div>
+        </div>
       )}
-      <div className="home__auth">
-        <div className="home__auth__login">
-          <button
-            className="home__auth__loginBtn"
-            onClick={() => goto("/login")}
-          >
-            Login
-          </button>
-        </div>
-        <div className="home__auth__signup">
-          <button
-            className="home__auth__signupBtn"
-            onClick={() => goto("/signup")}
-          >
-            Signup
-          </button>
-        </div>
-      </div>
     </div>
   );
 }
